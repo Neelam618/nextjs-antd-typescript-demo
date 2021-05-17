d4502d42bba5784e07d1de9c1fc32afa7e21342b
Neelam618 <neelamchavan410@gmail.com>
2021-05-17 19:41:32 +0530

    added navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,11 +11,11 @@ export default function MyApp({ Component, pageProps }) {
         NavMenuItems = {[
           { menuTxt: 'Web Themes and Templates', href: '#', key: 'key1' },
           { menuTxt: 'Code', href: '#', key: 'key2' },
-          { menuTxt: 'Video', href: '#', key: 'key2' },
-          { menuTxt: 'Audio', href: '#', key: 'key3' },
-          { menuTxt: 'Graphics', href: '#', key: 'key4' },
-          { menuTxt: 'Photos', href: '#', key: 'key5' },
-          { menuTxt: '3D Files', href: '#', key: 'key6' }
+          { menuTxt: 'Video', href: '#', key: 'key3' },
+          { menuTxt: 'Audio', href: '#', key: 'key4' },
+          { menuTxt: 'Graphics', href: '#', key: 'key5' },
+          { menuTxt: 'Photos', href: '#', key: 'key6' },
+          { menuTxt: '3D Files', href: '#', key: 'key7' }
         ]}
       />
   );
