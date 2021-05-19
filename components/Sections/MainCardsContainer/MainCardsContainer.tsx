adbd8997fef9e9d788401daa0de1358e531b0df2
Neelam618 <neelamchavan410@gmail.com>
2021-05-19 14:57:22 +0530

    main cards container styles updated

--- components/Sections/MainCardsContainer/MainCardsContainer.tsx
+++ components/Sections/MainCardsContainer/MainCardsContainer.tsx
@@ -22,10 +22,7 @@ type MainCardsContainerItemsType = {
 
 export const MainCardsContainer: FC<MainCardsContainerType> = ({ MainCardsContainerItems, btnTxt }) => {
   return (
-    <div 
-    // className="bg-gray-200 px-5 md:px-20 xl:px-30 py-10 lg:py-32"
-    style={{}}
-    >
+    <div>
       <Row gutter={[32, 32]}>
         {MainCardsContainerItems.map(
           ({
@@ -41,7 +38,7 @@ export const MainCardsContainer: FC<MainCardsContainerType> = ({ MainCardsContai
             cardImgAlt,
           }) => {
             return (
-              <Col md={12} xxl={8}>
+              <Col md={12} xl={8} style={{width: '100%'}}>
                 <Card
                   hoverable
                   // className="bg-gray-100 overflow-hidden shadow-sm hover:shadow-xl border-0 cursor-default hover:bg-white rounded-xl h-410px"
@@ -49,16 +46,16 @@ export const MainCardsContainer: FC<MainCardsContainerType> = ({ MainCardsContai
                 >
                   <div>
                     <a href={titleHref}>
-                      <h1 style={{fontSize: '32px', textAlign: 'center'}}>
+                      <h1 style={{fontSize: '32px', textAlign: 'center', fontWeight: 'bold'}}>
                         {titleTxt}
                       </h1>
                     </a>
-                    <p style={{textAlign: 'center', marginBottom: 8}}>{paraTxt}</p>
+                    <p style={{textAlign: 'center', marginBottom: 8, color: '#333333'}}>{paraTxt}</p>
                     <div className="text-center text-lg" style={{textAlign: 'center'}}>
-                      <a href={link1Href} style={{marginRight: 18, color: 'skyblue'}}>
+                      <a href={link1Href} style={{marginRight: 18, color: '#0982ae'}}>
                         {link1Txt}
                       </a>
-                      <a href={link2Href} style={{marginRight: 18, color: 'skyblue'}}>
+                      <a href={link2Href} style={{marginRight: 18, color: '#0982ae'}}>
                         {link2Txt}
                       </a>
                     </div>
@@ -66,15 +63,15 @@ export const MainCardsContainer: FC<MainCardsContainerType> = ({ MainCardsContai
 
                   <a href={titleHref} className="imgWrapper">
                     <div
-                      style={{ boxShadow: '0 0 0 4px white', transform: 'translateY(50%)', width:64, height: 64, top: 0, borderRadius: '50%', margin: '0 auto'}}
+                      style={{ transform: 'translateY(50%)', width:64, height: 64, top: 0, borderRadius: '50%', margin: '0 auto', boxShadow: '0 0 0 4px hsl(0deg 0% 100%)'}}
                     >
-                      <img src={iconImgSrc} alt="" className="rounded-full w-16 h-16" style={{borderRadius: '50%', width: 64, height: 64}} />
+                      <img src={iconImgSrc} alt="" className="rounded-full w-16 h-16" style={{borderRadius: '50%', width: 64, height: 64, boxShadow: 'white 0px 0px 0px 4px'}} />
                     </div>
                     <img
                       src={cardImgSrc}
                       alt={cardImgAlt}
                       // className="border-4 border-white border-solid shadow-2xl rounded-xl"
-                      style={{border: '4px solid white', display: 'block', margin: '0 auto', width: '100%'}}
+                      style={{border: '4px solid white', display: 'block', margin: '0 auto', width: '100%', borderRadius: 8, boxShadow: '0 20.83px 22.92px rgb(0 0 0 / 34%)'}}
                     />
                   </a>
                 </Card>
